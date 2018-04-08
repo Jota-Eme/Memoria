f4cd18013b8396cbd9af8ad0bbbba03a8f3d28f5
Jose-manuel Urtasun <josemanuel.urtasun.inf@gmail.com>
2018-04-08 11:40:30 -0700

    Se implemento el programa para graficar las instancias en python, se encuentra funcionando, falta solo agregar el input para recibir el nombre de la instancia, actualmente se encuentra fija.

--- GRASP VRPCD/asd.py
+++ GRASP VRPCD/asd.py
@@ -1,6 +1,57 @@
 import matplotlib.pyplot as plt
+
+with open('instances_MI/R1_4_1_200m.txt') as f:
+	read_data = f.readline()
+	read_data = f.readline() 
+
+	requests_number = int(read_data)
+
+	read_data = f.readline()	
+	read_data = f.readline()
+	read_data = f.readline()
+
+	crossdocks_number = int(read_data)
+
+	for i in range(8):
+		read_data = f.readline()
+
+	crossdock_x = []
+	crossdock_y = []
+
+	for i in range(crossdocks_number):
+		read_data = f.readline()
+		token = read_data.split(" ")
+		token = list(filter(None, token))
+		crossdock_x.append(int(token[1]))
+		crossdock_y.append(int(token[2]))
+
+	suplier_x = []
+	suplier_y = []
+	customer_x = []
+	customer_y = []
+
+	for i in range(requests_number):
+		read_data = f.readline()
+		token = read_data.split(" ")
+		token = list(filter(None, token))
+		suplier_x.append(int(token[1]))
+		suplier_y.append(int(token[2]))
+		customer_x.append(int(token[5]))
+		customer_y.append(int(token[6]))
+
+f.closed
+
+plt.scatter(crossdock_x,crossdock_y,color='green')
+plt.scatter(suplier_x,suplier_y,color='red')
+plt.scatter(customer_x,customer_y,color='blue')
+plt.show()
+
+
+
+
+'''
 year=[1950,1970,1990,2010]
 pop=[2.519,3.692,5.263,6.972]
 plt.scatter(year,pop)
-plt.show()
+plt.show()'''
 
